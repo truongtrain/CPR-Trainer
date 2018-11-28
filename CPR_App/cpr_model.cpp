c47b40b9c7b653e1d359f9cac3afd6dbb996266d
Tom <nguyentom1996@yahoo.com>
2018-11-27 21:43:09 -0700

    CPR Model now has a .h and .cpp for Qt

--- CPR_App/cpr_model.cpp
+++ CPR_App/cpr_model.cpp
@@ -1,7 +1,103 @@
 #include "cpr_model.h"
 
-
 CPR_Model::CPR_Model()
+{
+        isPatientConscious = false;
+        isPatientBreathing = false;
+        doesPatientHavePulse = false;
+
+        //  initialize the game to the beginning
+
+        currentState = CHECK_RESPONSIVENESS;
+}
+
+    //  this is a slot that listens to actions performed from the view
+void CPR_Model::actionPerformed(int action)
+{
+    if (action == currentState)
+       {
+         advanceSuccessfully();
+       }
+
+       else
+       {
+         actionFailed(action);
+       }
+}
+
+void CPR_Model::advanceSuccessfully(int action)
+{
+    if (currentState == CHECK_RESPONSIVENESS)
+        {
+          //    if the patient isn't conscious
+          if (!isPatientConscious)
+          {
+             emit changeStatusBoxSignal("The patient does not respond to you.");
+
+
+            //  start a timer that makes the game fail if it goes off before the player performs a successful action
+            cout << "Timer started waiting for the user to call for an AED." << endl;
+
+            currentState = CALL_FOR_911_AND_AED;
+            return;
+          }
+
+          //    if the patient is conscious
+          else
+          {
+            emit changeStatusBoxSignal("The patient wakes up and responds to you. Talk to them to see what they need. Game over, you win!");
+            emit gameOverWinSignal("Game over, you win!");
+
+
+            currentState = GAME_OVER;
+            return;
+          }
+        }
+
+        if (currentState == CALL_FOR_911_AND_AED)
+        {
+          emit changeStatusBoxSignal("Bystanders call 911 and start looking for an AED");
+          cout << "The patient does not respond to you." << endl;
+
+          //start a timer that makes the game fail if it goes off before the player performs a successful action;
+          cout << "Timer started waiting for the user to check pulse and breathing." << endl;
+
+          currentState = CHECK_PULSE_AND_BREATHING;
+          return;
+        }
+
+        if (currentState == CHECK_PULSE_AND_BREATHING)
+        {
+          //    if no pulse and no breathing
+          if (!doesPatientHavePulse && !isPatientBreathing)
+          {
+            emit changeStatusBoxSignal("Patient has no pulse and no breathing");
+
+          }
+
+          //    if has pulse and no breathing
+          if (doesPatientHavePulse && !isPatientBreathing)
+          {
+            emit changeStatusBoxSignal("Patient has a pulse, but is not breathing.");
+
+          }
+
+          //    if has pulse and is breathing
+          if (doesPatientHavePulse && isPatientBreathing)
+          {
+           emit changeStatusBoxSignal("Patient has a pulse and is breathing.");
+
+          }
+
+          //    start a timer that makes the game fail if it goes off before the player performs a successful action
+          cout << "Timer started waiting for user to give breaths or compressions." << endl;
+
+          currentState = CHECK_PULSE_AND_BREATHING;
+          return;
+        }
+}
+
+void CPR_Model::actionFailed(int action)
 {
 
 }
--- CPR_App/cpr_model.h
+++ CPR_App/cpr_model.h
@@ -1,11 +1,57 @@
 #ifndef CPR_MODEL_H
 #define CPR_MODEL_H
 
+#include <QObject>
+#include <iostream>
+using namespace std;
 
-class CPR_Model
+class CPR_Model : public QObject
 {
+    Q_OBJECT
+
 public:
     CPR_Model();
+
+public slots:
+    void actionPerformed(int);
+
+private:
+    int currentState;
+
+    const int CHECK_RESPONSIVENESS  = 0;
+    const int CALL_FOR_911_AND_AED = 1;
+    const int CHECK_PULSE_AND_BREATHING = 2;
+    const int GIVE_BREATH = 3;
+    const int PLACE_HANDS = 4;
+    const int GIVE_COMPRESSION = 5;
+    const int TURN_ON_AED = 6;
+    const int ATTATCH_AED_PAD_1 = 7;
+    const int ATTATCH_AED_PAD_2 = 8;
+    const int SHOUT_CLEAR_FOR_ANALYZE = 9;
+    const int PRESS_ANALYZE = 10;
+    const int SHOUT_CLEAR_FOR_SHOCK = 11;
+    const int PRESS_SHOCK = 12;
+    const int GAME_OVER = 13;
+
+    bool isPatientConscious;
+    bool doesPatientHavePulse;
+    bool isPatientBreathing;
+    bool isProMode;
+
+    int compressionsGiven;
+    int breathsGiven;
+
+
+    //  helper methods
+    void advanceSuccessfully(int action);
+    void actionFailed(int action);
+
+signals:
+    void changeStatusBoxSignal(string message);
+    void gameOverWinSignal(string message);
+
 };
 
 #endif // CPR_MODEL_H
+
+
